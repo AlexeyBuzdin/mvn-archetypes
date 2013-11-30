3aaf9226f109c70e375e4695c3d321c35aa1a090
LArchaon <alex.buzdin@gmail.com>
2013-11-30 13:50:25 +0200

    deleted extra files

--- vaadin/src/main/java/com/sample/vaadin/HelloVaadin.java
+++ vaadin/src/main/java/com/sample/vaadin/HelloVaadin.java
@@ -1,12 +1,14 @@
- package com.sample.vaadin;  
- import com.vaadin.Application;  
- import com.vaadin.ui.*;  
- public class HelloVaadin extends Application {  
-   @Override  
-   public void init() {  
-     Window mainWindow = new Window("HelloVaadin");  
-     Label label = new Label("Hello Vaadin user");  
-     mainWindow.addComponent(label);  
-     setMainWindow(mainWindow);  
-   }  
- }  
+package com.sample.vaadin;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+
+public class HelloVaadin extends Application {
+    @Override
+    public void init() {
+        Window mainWindow = new Window("HelloVaadin");
+        Label label = new Label("Hello Vaadin user");
+        mainWindow.addComponent(label);
+        setMainWindow(mainWindow);
+    }
+}
