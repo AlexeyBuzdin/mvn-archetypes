664ff67c9898384bab7bc95b7c7d1fedc9a4aa5a
LArchaon <alex.buzdin@gmail.com>
2013-11-30 13:33:49 +0200

    vaadin example added

---
+++ vaadin/src/main/java/com/sample/vaadin/HelloVaadin.java
@@ -0,0 +1,12 @@
+ package com.sample.vaadin;  
+ import com.vaadin.Application;  
+ import com.vaadin.ui.*;  
+ public class HelloVaadin extends Application {  
+   @Override  
+   public void init() {  
+     Window mainWindow = new Window("HelloVaadin");  
+     Label label = new Label("Hello Vaadin user");  
+     mainWindow.addComponent(label);  
+     setMainWindow(mainWindow);  
+   }  
+ }  
